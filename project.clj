58e5010e9cd7adc7a48c6a87d6d6841128fff2e8
Jan Stępień <jstepien@users.sourceforge.net>
2012-06-11 13:04:33 +0200

    Set groupId to org.clojure.jan

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject himilsbach "0.0.0-SNAPSHOT"
+(defproject org.clojure.jan/himilsbach "0.0.0-SNAPSHOT"
   :description "A tiny actor library for Clojure"
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure/core.match "0.2.0-alpha9"]])
