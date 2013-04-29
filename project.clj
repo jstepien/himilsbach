ccc0447c1157f0c870dbb7b8f1c0df52e2b68fb0
Jan Stępień <jstepien@users.sourceforge.net>
2013-04-29 21:18:45 +0200

    Add :url and :license to project.clj

--- project.clj
+++ project.clj
@@ -1,5 +1,8 @@
 (defproject org.clojure.jan/himilsbach "0.0.1-SNAPSHOT"
   :description "A tiny actor library for Clojure"
+  :url "https://github.com/jstepien/himilsbach"
+  :license {:name "MIT License"
+            :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure/core.match "0.2.0-alpha9"]])
 
