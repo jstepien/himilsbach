ed94034325b8e015f5a25687014de5a583d991ab
Jan Stępień <jstepien@users.sourceforge.net>
2013-05-14 19:37:29 +0200

    Bump version to 0.1.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject himilsbach "0.1.0"
+(defproject himilsbach "0.1.1-SNAPSHOT"
   :description "A tiny actor library for Clojure"
   :url "https://github.com/jstepien/himilsbach"
   :license {:name "MIT License"
